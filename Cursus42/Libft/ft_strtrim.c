4b3bb8c5f1e13cb8deaecad0bd6cde50ea1ba121
gamer0ver94 <duarte.paulino94@hotmail.com>
2022-03-04 02:29:59 +0100

    lwq

--- Cursus42/Libft/ft_strtrim.c
+++ Cursus42/Libft/ft_strtrim.c
@@ -11,51 +11,22 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-static int countfunction(char *s1, char *set)
+static int mallocount(char *s1, char *set)
 {
      int i;
     int j;
     int count;
+    int size;
 
     j = 0;
     i = 0;
     count = 0;
-    while(s1[i])
+    size = ft_strlen(set) - 1;
+    while (s1[i])
     {
-       while(s1[i] == set[j])
-       {
-           i++;
-           j++;
-           count++;
-       }
-       if(s1[i] != ft_strlen(set))
-       {
-           count = count -j;
-           j = 0;
-       }
-       count++;
-       i++;
-    }
-    return(count);
-}
-char *ft_strtrim(char const *s1, char const *set)
-{
-    int i;
-    int j;
-    int h;
-    char *newstr;
-    int size;
-
-    h = 0;
-    newstr = (char *)malloc(sizeof(char) * 10);
-    j = 0;
-    i = 0;
-    size = ft_strlen(set);
-    while (s1[i] - 1)
-    {
-        while(s1[i + 1] == set[j] && s1[i + size] == set[size)])
+        while(s1[i] == set[j] && s1[i + size] == set[size])
         {
-            if(s1[i + size] == set[size]))
+            if(s1[i + size] == set[size])
             {
                 size--;
             }
@@ -66,49 +37,43 @@ char *ft_strtrim(char const *s1, char const *set)
                 size = size - 1;
             }
         }
-        newstr[h] = s1[1];
+        count++;
         i++;
+    }
+   return(count);
+}
+char *ft_strtrim(char const *s1, char const *set)
+{
+    int i;
+    int j;
+    int h;
+    char *newstr;
+    int size;
+
+    h = 0;
+    newstr = (char *)malloc(sizeof(char) * mallocount((char *)s1, (char *)set));
+    j = 0;
+    i = 0;
+    size = ft_strlen(set) -1;
+    while (s1[i])
+    {
+        while(s1[i] == set[j] && s1[i + size] == set[size])
+        {
+            if(s1[i + size] == set[size])
+            {
+                size--;
+            }
+            if(size == 0)
+            {
+                size = ft_strlen(set);
+                i = i + size;
+                size = size - 1;
+            }
+        }
+        newstr[h] = s1[i];
+        i++;
+        h++;
     }
 }
 
 
-
-
-
-
-
-///test
-
-// char *ft_strtrim(char const *s1, char const *set)
-// {
-//     int i;
-//     int j;
-//     int h;
-//     char *newstr;
-//     int size;
-
-//     h = 0;
-//     newstr = (char *)malloc(sizeof(char) * 20);
-//     j = 0;
-//     i = 0;
-//     size = strlen(set) - 1;
-//     while (s1[i])
-//     {
-//         while((s1[i] == set[j]) && (s1[i + size] == set[size]))
-//         {
-//             if(s1[i + size] == set[size])
-//             {
-//                 size--;
-//             }
-//             if(size == 0)
-//             {
-//                 size = strlen(set);
-//                 i = i + size;
-//                 size = size - 1;
-//             }
-//         }
-//         newstr[h] = s1[i];
-//         h++;
-//         i++;
-//     }
-// }
