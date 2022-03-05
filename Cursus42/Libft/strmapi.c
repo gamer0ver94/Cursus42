834c014ba1ed5fbb19986655a96437b12266361d
gamer0ver94 <duarte.paulino94@hotmail.com>
2022-03-05 15:02:54 +0100

    libft.h modified

---
+++ Cursus42/Libft/strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strmapi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/05 02:18:28 by marvin            #+#    #+#             */
+/*   Updated: 2022/03/05 02:18:28 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char *newstr;
+    int i;
+
+    i = 0;
+    newstr = ft_strdup(s);
+    if(!newstr)
+    {
+        return (NULL);
+    }
+    while(newstr[i])
+    {
+        newstr[i] = (*f)(i, newstr[i]);
+        i++;
+    }
+    return (newstr);
+}
