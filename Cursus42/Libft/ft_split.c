f7fb39d280ff882da1ea976ee2e90200a05b5830
gamer0ver94 <duarte.paulino94@hotmail.com>
2022-03-14 00:06:41 +0100

    warking

--- Cursus42/Libft/ft_split.c
+++ Cursus42/Libft/ft_split.c
@@ -1,3 +1,160 @@
 #include "libft.h"
+char numberofwords(char const *s, char c)
+{
+    int i;
+    int words;
 
+    words = 0;
+    i = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] = c)
+        {
+            i++;
+        }
+        if (s[i] != c)
+        {
+            if(s[i + 1] == c || s[i + 1] == '\0')
+            {
+                words++;
+            }
+            i++;
+        }
+    }
+    return (words);
+}
+int wordlenght(char const *s, char c)
+{
+    int j;
+    int i;
+
+    i = 0;
+    j = 0;
+    while (s[i] == c)
+    {
+        i++;
+    }
+    while (s[ i + j] != c) //not sure
+    {
+        j++;
+    }
+    return (j);
+}
+int allocatecpy(char const *s, char **str, char c)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while (i < numberofwords(s,c))
+    {
+        str[i] = malloc(sizeof(char) * wordlenght(s[i] + j, c));
+        j = wordlenght(s[i] + j, c);
+        i++;
+    }
+}
 char **ft_split(char const *s, char c)
+{
+    int i = 0;
+    char **str;
+    str = malloc(sizeof(char) *numberofwords(s,c));
+    while (i < numberofwords(s,c))
+    {
+        allocatecpy(s,str,c);
+        i++;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char numberofwords(char const *s, char c)
+{
+    int i;
+    int words;
+
+    words = 0;
+    i = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] == c)
+        {
+            i++;
+        }
+        if (s[i] != c)
+        {
+            if(s[i + 1] == c || s[i + 1] == '\0')
+            {
+                words++;
+            }
+            i++;
+        }
+    }
+    return (words);
+}
+int wordlenght(char const *s, char c, int index)
+{
+    int j;
+
+    j = 0;
+    while (s[index] == c)
+    {
+        index++;
+    }
+    while (s[ index + j] != c)
+    {
+        j++;
+    }
+    return (j);
+}
+char **allocatecpy(char const *s, char **str, char c, int words)
+{
+    int i;
+    int index;
+
+    i = 0;
+    index = wordlenght(s, c, 0);
+    while (i < words)
+    {
+        str[i] = malloc(sizeof(char) * index);
+        index = wordlenght(s[i] + index, c, index);
+        copyword(s,str[i],c,index);
+        i++;
+    }
+    return (str);
+}
+char **ft_split(char const *s, char c)
+{
+    int i = 0;
+    char **str;
+    int words;
+
+    words =  numberofwords(s,c);
+    str = malloc(sizeof(char) *words);
+    while (i < words)
+    {
+        allocatecpy(s,str[i],c,words);
+        i++;
+    }
+}
+int main() {
+  **str = ft_split(" jk jk", ' ');
+  while (i < 2)
+  {
+      printf("%s",str[i])
+      i++;
+  }
+  return 0;
+}
--- Cursus42/Libft/ft_strtrim.c
+++ Cursus42/Libft/ft_strtrim.c
@@ -6,73 +6,29 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 16:20:13 by dpaulino          #+#    #+#             */
-/*   Updated: 2022/03/08 04:28:17 by marvin           ###   ########.fr       */
+/*   Updated: 2022/03/11 10:05:39 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-static int mallocount(char *s1, char *set)
-{
-     int i;
-    int j;
-    int count;
-    int size;
 
-    j = 0;
-    i = 0;
-    count = 0;
-    size = ft_strlen(set) - 1;
-    while (s1[i])
-    {
-        while(s1[i] == set[j] && s1[i + size] == set[size])
-        {
-            if(s1[i + size] == set[size])
-            {
-                size--;
-            }
-            if(size == 0)
-            {
-                size = ft_strlen(set);
-                i = i + size;
-                size = size - 1;
-            }
-        }
-        count++;
-        i++;
-    }
-   return(count);
-}
 char *ft_strtrim(char const *s1, char const *set)
 {
-    int i;
-    int j;
-    int h;
-    char *newstr;
-    int size;
-
-    h = 0;
-    newstr = (char *)malloc(sizeof(char) * mallocount((char *)s1, (char *)set));
-    j = 0;
-    i = 0;
-    size = ft_strlen(set) -1;
-    while (s1[i])
-    {
-        while(s1[i] == set[j] && s1[i + size] == set[size])
-        {
-            if(s1[i + size] == set[size])
-            {
-                size--;
-            }
-            if(size == 0)
-            {
-                size = ft_strlen(set);
-                i = i + size;
-                size = size - 1;
-            }
-        }
-        newstr[h] = s1[i];
-        i++;
-        h++;
-    }
-    return (newstr);
+	size_t i;
+	
+	if(!s1 || !set)
+	{
+		return (NULL);
+	}
+	while (*s1 && ft_strchr(set, *s1))
+	{
+		s1++;
+	}
+	i = ft_strlen(s1);
+	while(i && ft_strchr(set,s1[i]))
+	{
+		i--;
+	}
+	
+	return(ft_substr(s1, 0 , i + 1));
 }
