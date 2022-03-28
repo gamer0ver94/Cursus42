803fa644c505a6c7a2c264059e6a9717557d75b3
gamer0ver94 <duarte.paulino94@hotmail.com>
2022-03-28 14:35:45 +0200

    idsa

--- Cursus42/get_next_line/get_next_line.c
+++ Cursus42/get_next_line/get_next_line.c
@@ -1,6 +1,25 @@
 #include "get_next_line.h"
+#include "stdio.h"
+
+char	*read_line(fd,buffer)
+{
+	int bytes; //bytes to read
+	int i;
+	
+	// i = 0;
+	bytes = 1;
+	read(fd, buffer, bytes);
+	printf("%s",buffer);
+	return(buffer);
+}
 
 char	*get_next_line(int fd)
 {
-
+	char *buffer; //contain the save
+	if (!fd)
+	{
+		return (0);
+	}
+	read_line(fd);
+	return (buffer);
 }
---
+++ Cursus42/get_next_line/main.c
@@ -0,0 +1,14 @@
+#include "get_next_line.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+int	main(void)
+{
+	int file;
+	char	buffer[40];
+
+	file = open("testing.txt", O_RDONLY);
+	get_next_line(file);
+	return (0);
+}
